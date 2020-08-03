326294508bb18f6a402631b4b426b663fc620c8f
boberro <m.cyraniak@gmail.com>
2020-08-03 23:40:20 +0200

    fix: commented out custom loadout

--- loadouts/_loadouts.hpp
+++ loadouts/_loadouts.hpp
@@ -1,10 +1,10 @@
 #include "_macros.hpp"
 
-class CfgLoadouts
-{
-    class FIA_TS {
-        displayName = "3IFB (Tiger Stripe)";
-        category = "3IFB";
-        #include "fia_ts.hpp"
-    };
-};
+// class CfgLoadouts
+// {
+//     class FIA_TS {
+//         displayName = "3IFB (Tiger Stripe)";
+//         category = "3IFB";
+//         #include "fia_ts.hpp"
+//     };
+// };
